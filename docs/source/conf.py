405ee759fa9a09ac96b14099f5eca7998e1ef54e
Alla Khvatova <60952567+AllaKhvatova@users.noreply.github.com>
2021-10-28 21:30:55 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Matagami'
+copyright = '2021, DGT Network'
+author = 'Alla Khvatova'
 
 release = '0.1'
 version = '0.1.0'
